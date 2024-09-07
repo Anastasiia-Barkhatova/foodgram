c262a4ff7bbb0ca1486687304d84029642ffcabc
Anastasiia Barkhatova <nastyabarhatova@mail.ru>
2024-09-07 16:07:49 +0300

    отредактирован main.yml

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator
 from django.core.validators import RegexValidator
-from django.urls import reverse
 
 from recipes.constants import (
     INGREDIENT_NAME_LENGHT,
